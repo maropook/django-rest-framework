d7e168036bea9d59b14f81814deeaacd22743be6
maropook <ii440092@icloud.com>
2021-08-28 07:23:24 +0900

    first

---
+++ django_rest_framework/blog/serializer.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import User, Entry
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'mail')
+
+
+class EntrySerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
+    class Meta:
+        model = Entry
+        fields = ('title', 'body', 'created_at', 'status', 'author')
